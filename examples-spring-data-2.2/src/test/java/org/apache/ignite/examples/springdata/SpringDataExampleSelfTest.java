2d637b05aaecb45e58c196138afbbeb139cb80a0
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2026-09-16 18:35:49 +0400

    GG-50709 Rename ignite-spring-data_2.2 to ignite-spring-data

---
+++ examples-spring-data-2.2/src/test/java/org/apache/ignite/examples/springdata/SpringDataExampleSelfTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ignite.examples.springdata;
+
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
+import org.junit.Test;
+
+/**
+ * Spring Data example test.
+ */
+public class SpringDataExampleSelfTest extends GridAbstractExamplesTest {
+    /**
+     * @throws Exception If failed.
+     */
+    @Test
+    public void testSpringDataExample() throws Exception {
+        SpringDataExample.main(EMPTY_ARGS);
+    }
+}
--- examples-spring-data/src/test/java/org/apache/ignite/examples/springdata/SpringDataExampleSelfTest.java
+++ examples-spring-data/src/test/java/org/apache/ignite/examples/springdata/SpringDataExampleSelfTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 GridGain Systems, Inc. and Contributors.
+ * Copyright 2026 GridGain Systems, Inc. and Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.ignite.examples;
+package org.apache.ignite.examples.springdata;
 
-import org.apache.ignite.examples.springdata.SpringDataExample;
 import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
 import org.junit.Test;
 
